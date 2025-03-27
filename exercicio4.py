be481d1775d698e6b8917d204407de42287b0ead
João V. A. Carvalho <jv.carvalho.cv@gmail.com>
2025-03-27 18:50:48 -0300

    Delete exercicio4.py

--- exercicio4.py
+++
@@ -1,6 +0,0 @@
-num = int(input("insira um valor numério inteiro: "))%2
-# a variavel num recebe um valor inserido pelo usuario que também na mesma linha faz módulo de 2
-if num == 0:
-    print("O número inserido é par")
-else:
-    print("O número inserido é impar")
