8b8d3a3eaa70cff15cd0c29ea44b91ff0922afd5
João V. A. Carvalho <jv.carvalho.cv@gmail.com>
2025-03-27 18:50:40 -0300

    Delete exercicio3.py

--- exercicio3.py
+++
@@ -1,6 +0,0 @@
-x = int(input("Insira um número inteiro:"))
-y = int(input("Insira um número inteiro:"))
-soma = x+y
-sub = x-y
-mult = x*y
-print(f"soma: {soma}, subtração: {sub}, multiplicação: {mult}")
