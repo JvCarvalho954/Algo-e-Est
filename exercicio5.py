1642fc380975974b4bb3b3a2ada5e3d535f49c9e
João V. A. Carvalho <jv.carvalho.cv@gmail.com>
2025-03-27 18:51:00 -0300

    Delete exercicio5.py

--- exercicio5.py
+++
@@ -1,8 +0,0 @@
-num = int(input("insira um valor inteiro"))
-if not num == 0:
-    if num > 0:
-        print("número positivo")
-    else:
-        print("número negativo")
-else:
-    print("número é zero")
